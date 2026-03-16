9cbcaf2a923062aac4de7a75fba294c44596f990
yakkovwaxelbom <0583281019v@gmail.com>
2026-03-16 12:02:34 +0200

    finish kafka consumer connection

---
+++ shard/config/kafka_consumer.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+class KafkaConsumerConfig(BaseSettings):
+    BOOTSTRAP_SERVERS: str = '127.0.0.1:9092'
+    GROUP_ID: str = 'intel_worker_v:1'
+    ENABLE_AUTO_COMMIT: bool = False
+    AUTO_OFFSET_RESET: str = 'earliest'
+    TOPIC_ERROR: str = 'intel_signals_dlq'
+
+    def to_dict(self):
+        return {
+            k.replace('_', '.').lower(): v
+            for k, v in self.model_dump().items}
---
+++ shard/connection/kafka_consumer.py
@@ -0,0 +1,127 @@
+import logging
+from confluent_kafka import Consumer, Producer, Message, KafkaError
+from typing import Dict, Callable, List
+import json
+
+
+logger = logging.getLogger(__name__)
+
+
+class MsgError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+    def __str__(self):
+        return f"{self.message}"
+
+class KafkaConsumerConnection:
+
+    _instance = None
+    _initialized = False
+
+
+    def __new__(cls, **config):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self, **config):
+        if not self._initialized:
+
+            producer_config = {
+                'bootstrap.servers': config['bootstrap.servers']
+            }
+
+            self._error_topic = config.pop('topic.error')
+
+            self._client = Consumer(config)
+            self._func_event_loop: Callable = None
+
+            self._producer_errors = Producer(producer_config)
+
+            self._initialized = True
+
+
+    def subscribe(self, topics: List[str]):
+        self._client.subscribe(topics)
+
+
+    def register_func(self, handle: str, func: Callable):
+        self._func_event_loop = func
+
+
+    def start_event_loop(self, num_commit_count=10):
+        try:
+            msg_count = 0
+
+            while True:
+
+                try:
+
+                    msg = self._client.poll(1.0)
+
+                    if not msg: continue
+
+                    if self._handle_errors(msg): continue
+
+                    self._msg_process(msg)
+
+                    msg_count += 1
+
+                    if msg_count == num_commit_count:
+                        self._client.commit(asynchronous=False)
+                        msg_count = 1
+
+                except MsgError as e:
+
+                    value = {'msg': msg,
+                             'error': str(e)}
+                    
+                    value = json.dumps(value).encode("utf-8")
+
+                    self._producer_errors.produce(topic=self._error_topic,
+                                                  value=value,
+                                                  callback=self._delivery_error_msg_report)
+
+        except KeyboardInterrupt:
+            logger.info('Stopping consumer"')
+
+        finally:
+            self._client.close()
+
+
+    def _handle_errors(msg: Message):
+        if e := msg.error():
+
+            if e.code() == KafkaError.UNKNOWN_TOPIC_OR_PART:
+                logger.warning(f'unknown topic: {msg.topic} or part: {msg.partition}')
+         
+            else:
+                logger.error('unknown error')
+
+        return e != None
+    
+    def _msg_process(self, msg: Message):
+
+        data_str = msg.value().decode(encoding='utf-8')
+
+        try:
+            data_dict: Dict = json.loads(data_str)
+
+            self._func_event_loop(data_dict)
+
+        except json.decoder.JSONDecodeError as e:
+            raise MsgError(str(e))
+
+
+    def _delivery_error_msg_report(err, msg: Message):
+        if err:
+            logger.error(f"{msg.value().decode()} Delivery error failed: {err}")
+        else:
+            logger.warning(f"Error delivered {msg.value().decode("utf-8")}")
+
+
+
+
+
+         
