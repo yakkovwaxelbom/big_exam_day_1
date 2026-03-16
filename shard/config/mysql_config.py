cdc0dd1f370640740eae32b8a01a2404c86a1875
yakkovwaxelbom <0583281019v@gmail.com>
2026-03-16 12:26:45 +0200

    finish mysql connection

---
+++ shard/config/mysql_config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+class KafkaConsumerConfig(BaseSettings):
+    MYSQL_HOTS: str = '127.0.0.1'
+    MYSQL_POR: int = 3306
+    MYSQL_USR: str = 'root'
+    MYSQL_PASSWORD: str = '123456'
+    MYSQL_DATABASE: str = 'dev'
+
+    def to_dict(self):
+        return {
+            k.replace('MYSQL_', '.').lower(): v
+            for k, v in self.model_dump().items()}
---
+++ shard/connection/mysql.py
@@ -0,0 +1,53 @@
+from mysql.connector import pooling, MySQLConnection
+from contextlib import contextmanager
+
+
+class MySqlConnection:
+
+    _instance = None
+    _initialized = False
+
+
+    def __new__(cls, **config):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self, **config):
+        if not self._initialized:
+
+            self._pool = pooling.MySQLConnectionPool(
+                pool_name='dev',
+                pool_size=3,
+                **config
+            )
+
+            self._initialized = True
+
+
+    def get_connection(self):
+        return self._pool.get_connection()
+    
+    @contextmanager
+    def get_cursor(self):
+        conn = None
+        cursor = None
+
+        try:
+            conn: MySQLConnection = self.get_connection()
+            cursor = conn.cursor(dictionary=True)
+
+            yield cursor
+
+            conn.commit()
+
+        except Exception as e:
+            if conn:
+                conn.rollback()
+
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
+
